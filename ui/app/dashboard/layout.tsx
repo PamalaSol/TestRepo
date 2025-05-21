35a68e8f52259ba3f0a10b5dba44bfd998154777
Petar <taneski001@gmail.com>
2025-05-21 17:03:52 +0200

    no message

--- ui/app/dashboard/layout.tsx
+++ ui/app/dashboard/layout.tsx
@@ -2,21 +2,29 @@
 import Sidebar from '@/app/dashboard/_components/Sidebar';
 import { ToastContainer } from 'react-toastify';
 import './globals.css';
+import { Metadata } from 'next';
 
 import LangProvider from './_context/LangContext';
 import Header from './_components/Header';
 import { usePathname } from 'next/navigation';
 
+export const metadata: Metadata = {
+	robots: {
+		index: false,
+		follow: false,
+	},
+};
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
 	const pathname = usePathname();
 
-	const defaultLayout = (<div>{children}</div>);
+	const defaultLayout = <div>{children}</div>;
 	const dashboardLayout = (
 		<div className="grid h-screen grid-cols-12">
-			<div className="col-span-2 h-full w-full">
+			<div className="w-full h-full col-span-2">
 				<Sidebar />
 			</div>
-			<div className="container col-span-10 h-full w-full px-8 py-10">
+			<div className="container w-full h-full col-span-10 px-8 py-10">
 				<Header />
 				<div className="h-fit">{children}</div>
 			</div>
@@ -27,11 +35,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
 		<div>
 			<LangProvider>
 				<title>Ho-Matic Dashboard</title>
-				{pathname === '/dashboard/login' ? (
-					defaultLayout
-				) : (
-					dashboardLayout
-				)}
+				{pathname === '/dashboard/login' ? defaultLayout : dashboardLayout}
 			</LangProvider>
 			<ToastContainer />
 		</div>
