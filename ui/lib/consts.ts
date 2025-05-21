d0dc1eccbab170bfdbe35deb8d3df5f7b0337f04
Petar <taneski001@gmail.com>
2025-05-21 15:58:15 +0200

    no message

--- ui/lib/consts.ts
+++ ui/lib/consts.ts
@@ -1,1 +1,2 @@
-export const baseUrl = 'https://test-repo-ngt8sx2aw-pamalasols-projects.vercel.app';
+export const baseUrl = process.env.NEXT_PUBLIC_GENERAL_URL || 'https://ho-matic.ch';
+// Fallback to production if the env var is not set, though it should always be set in Vercel environments.
